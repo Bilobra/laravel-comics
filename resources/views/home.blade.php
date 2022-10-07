9ef4f2e493f6a08c579fff68cf2053e1f38b7755
Bilobra <bra.carrozzo@gmail.com>
2022-10-07 16:39:38 +0200

    fix style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="black_section">
         <div class="container text_welcome">
-            Benvenuti sul nostro sito, per visualizzare i fumetti clicca qui --> <strong><a href="/comics">/COMICS</a></strong>.
+            Benvenuti sul nostro sito, per visualizzare i fumetti clicca qui --> <strong><a class="link_comics" href="/comics">/COMICS</a></strong>.
         </div>
     </div>
 @endsection
